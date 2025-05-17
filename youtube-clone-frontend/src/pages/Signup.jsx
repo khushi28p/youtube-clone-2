c74449b18647c21a2b71b636b210ae3f862c6f20
khushi28p <162727674+khushi28p@users.noreply.github.com>
2025-05-17 16:09:56 +0530

    minor frontend changes

--- youtube-clone-frontend/src/pages/Signup.jsx
+++ youtube-clone-frontend/src/pages/Signup.jsx
@@ -2,11 +2,16 @@ import React, { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import Login from "./Login";
+import { useNavigate } from "react-router-dom";
 
 const Signup = () => {
     const [channelName, setChannelName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [message, setMessage] = useState("");
+    const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
     const handleSubmit = async(e) => {
         e.preventDefault();
@@ -16,12 +21,26 @@ const Signup = () => {
                 email,
                 password,
             });
-            console.log(response.data.message);
+            setMessage(response.data.message);
             if(response.status === 201){
-                alert("Signup successful");
+                alert("Signup successful!! Redirecting to login...");
+                setChannelName("");
+                setEmail("");
+                setPassword("");
+                setLoading(false);
+                setTimeout(() => {
+                  navigate("/login");
+                }, 1500);
             }
         } catch(error){
             console.log("Signup failed", error);
+            if(error.response?.data?.errors){
+              setError(error.response.dara.errors.map(err => err.msg).join(", "));
+            } else{
+              setError(error.response?.data?.message);
+            }
+            
+            setLoading(false);
         }
     }
   return (
@@ -40,8 +59,10 @@ const Signup = () => {
           <span className="text-sm font-semibold">Password</span>
           <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} className="border rounded-md px-2 py-1" />
         </label>
-        <button type="submit" className="border px-4 py-1 my-6 rounded-md bg-blue-600">Signup</button>
+        <button type="submit" className={`border px-4 py-1 my-6 rounded-md bg-blue-600 ${loading ? 'opacity-50 cursor-not-allowed' : ''}`} disabled={loading}>{loading ? "Signing up..." : "Signup"}</button>
       </form>
+      {message && <p className="bg-green-200 text-green-900 px-8 py-2 rounded-lg mt-4 text-center">{message}</p>}
+      {error && <p className="bg-red-200 text-red-900 px-8 py-2 rounded-lg mt-4 text-center">{error}</p>}
       <p>Already have an account? <Link to="/login">Login</Link></p>
       
     </div>
